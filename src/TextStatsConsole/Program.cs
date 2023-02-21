ea0db2e80070e29b9885d4de99cffc3018c0a4de
Justin Wright <justin.wright@outlook.com>
2023-02-21 14:06:31 -0800

    temp branch for autocomplete impl

--- src/TextStatsConsole/Program.cs
+++ src/TextStatsConsole/Program.cs
@@ -5,26 +5,15 @@ internal class Program
 {
     static void Main(string[] args)
     {
-
+        
         var library = new WordLibrary();
-        var input = "this is the list of valid words oh thin friend of mine!";
+        library.Build(FileUtility.ParseFileByLine(@"C:\Users\Justi\Projects\TextStats\TextStats\engmix.txt"));
 
-        foreach(var item in input.Split(' '))
-        {
-            library.AddWord(item);
-        }
+        var autoComplete = new AutoComplete(library);
+        Console.WriteLine("Type to suggest a word");
+        var input = Console.ReadLine();
+        var suggestion = autoComplete.SuggestWord(input);
 
-            
-        var validator = new WordValidator(library);
- 
-
-
-        Console.WriteLine(validator.IsValid("the"));
-        Console.WriteLine(validator.IsValid("this"));
-        Console.WriteLine(validator.IsValid("th"));
-        Console.WriteLine(validator.IsValid("mine"));        
-        Console.WriteLine(validator.IsValid("mine!"));
-        Console.WriteLine(validator.IsValid("thine"));
-        Console.WriteLine("Hello World!");
+        Console.WriteLine($"Suggested word is {suggestion}");
     }
 }
--- src/TextStatsCore/AutoComplete.cs
+++ src/TextStatsCore/AutoComplete.cs
@@ -11,6 +11,10 @@ public class AutoComplete
 
     public string SuggestWord(string intput)
     {
+        var rootNode = this.wordLibrary.GetWordTrie();
+
+
+
         return "";
     }
 
@@ -27,6 +31,47 @@ public class AutoComplete
     public void AddWord(string word)
     {
         this.wordLibrary.AddWord(word);
+    }
+
+    private static string GetSuggestion(CharacterNode node, string inputString, string suggestion)
+    {        
+        var numberOfChildNodes = currentNode.NextLetters.Count;
+        var remainingLetters = text.Length;
+        if(remainingLetters == 0)
+        {
+            if(numberOfChildNodes == 0 || currentNode.IsEndOfWordCharacter)            
+            {
+                return suggestion;
+            }
+
+            return false;
+        }
+
+
+
+        var firstLetter = text.First();
+
+        if(firstLetter == '.')
+        {
+            foreach(var childNode in currentNode.NextLetters.Values)
+            {
+                var substring = text.Substring(1); 
+                if(IsTextValid(childNode, substring))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        CharacterNode node;
+        if (!currentNode.NextLetters.TryGetValue(firstLetter, out node))
+        {
+            return false;
+        }   
+
+        return IsTextValid(node, text.Substring(1));
     }
 
 
